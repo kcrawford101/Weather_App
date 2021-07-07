8d3c5201f672a71fcbca1df9936922a90fb241d6
Kevin Crawford <crawfordkevin101@gmail.com>
2021-07-07 19:45:04 -0400

    created weather fetch request

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// API key and fetch request
 let weather = {
-    "apiKey": "b97e0971f053e47fa5a7a92a78b0c889"
-}
+    apiKey: "b97e0971f053e47fa5a7a92a78b0c889",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&appid=" 
+            + this.apiKey
+        )
+            .then ((response) => response.json())
+            .then ((data) => console.log(data))
+    },
+    
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, condition } = data.weather;
+        const { temp, humid } =
+
+    }
+};
